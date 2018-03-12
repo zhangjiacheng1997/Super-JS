44f8dbfb6dcfc3e7d8628ec0d5d58f3b4b38bfd8
zhangjiacheng1997 <286801343@qq.com>
2018-03-12 15:04:19 +0800

    课堂练习

--- LS02/数据转换.js
+++ LS02/数据转换.js
@@ -1,3 +1,15 @@
 if(new Boolean(false)){
     console.log("执行");
 }   //输出执行，因为对象转为布尔值总为true
+var temp="23"-1;
+typeof temp;
+//"number"
+var temp=23-"1";
+typeof temp;
+//"number"
+var temp=23+"1";
+typeof temp;
+//"string"
+var temp="23"+1;
+typeof temp;
+//"string"
