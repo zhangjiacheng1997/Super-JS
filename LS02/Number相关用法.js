fa9833bb6cfabf352278842c5d4d188c51fe92e1
zhangjiacheng1997 <286801343@qq.com>
2018-03-13 16:37:01 +0800

    课下练习

---
+++ LS02/Number相关用法.js
@@ -0,0 +1,39 @@
+//数据转换
+//------------------------------------------------------------//
+//其他类型转换成Boolean类型
+//转换方式：Boolean(); value?true:false; !!value
+Boolean(undefined);//False
+Boolean(null);//False
+Boolean(NaN);Boolean(0);//False,其他数字都是True
+Boolean("");//False,其他字符串都是True包括空格字符串
+//对象转化为布尔值总是true
+if(new Boolean(false)){
+    console.log("执行");
+}   //输出执行，因为对象转为布尔值总为true
+//------------------------------------------------------------//
+//其他类型转换成Number类型
+//转换方式：Number(); -value; parseFloat; parseInt;
+Number(undefined);//NaN
+Number(null);//0
+//转换布尔值时，false为0，true为1
+//转换字符串时，空字符串为0;解析字符串中的数字，若有字母转换为NaN;可以忽略开头和结尾的空格，但中间有空格是转换成NaN
+//------------------------------------------------------------//
+//其他类型转换成String类型
+//转换方式：String(); +value; value.toString();
+String(undefined)//"undefined"
+String(null)//"null"
+String(true)//"true"
+String(false)//"false"
+
+var temp="23"-1;
+typeof temp;
+//"number"
+var temp=23-"1";
+typeof temp;
+//"number"
+var temp=23+"1";
+typeof temp;
+//"string"
+var temp="23"+1;
+typeof temp;
+//"string"
