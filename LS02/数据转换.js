51624ea3782e9fab52330b1dacd08888686aedec
zhangjiacheng1997 <286801343@qq.com>
2018-03-13 22:44:21 +0800

    课下练习

--- LS02/数据转换.js
+++ LS02/数据转换.js
@@ -1,5 +1,7 @@
 //数据转换
+
 //------------------------------------------------------------//
+
 //其他类型转换成Boolean类型
 //转换方式：Boolean(); value?true:false; !!value
 Boolean(undefined);//False
@@ -10,14 +12,18 @@ Boolean("");//False,其他字符串都是True包括空格字符串
 if(new Boolean(false)){
     console.log("执行");
 }   //输出执行，因为对象转为布尔值总为true
+
 //------------------------------------------------------------//
+
 //其他类型转换成Number类型
 //转换方式：Number(); -value; parseFloat; parseInt;
 Number(undefined);//NaN
 Number(null);//0
 //转换布尔值时，false为0，true为1
 //转换字符串时，空字符串为0;解析字符串中的数字，若有字母转换为NaN;可以忽略开头和结尾的空格，但中间有空格是转换成NaN
+
 //------------------------------------------------------------//
+
 //其他类型转换成String类型
 //转换方式：String(); +value; value.toString();
 String(undefined)//"undefined"
