35b340b6030116d49c5518d6db74839b84524fee
zhangjiacheng1997 <286801343@qq.com>
2018-03-23 22:52:06 +0800

    课下练习

--- LS06/运算符.js
+++ LS06/运算符.js
@@ -1,3 +1,59 @@
-//==若数据类型不一样先转换数据类型。引用数据类型转换成基本数据类型
-var score = 68;
-console.log((score>90&&"优")||(score>75&&"良")||(score>60&&"及格")||(score>0&&"不及格"));
+//函数定义的三种方式
+/*用构造函数创建函数对象，参数都是字符串类型，最后一个参数是函数体*/
+var str = "return a<b?a:b";
+var min = new Function("a","b",str);//动态生成函数，可以随时给str传值
+/*用关键字创建函数*/
+function min(){
+    return a<b?a:b;
+}
+/*利用变量*/
+var min = function(){
+    return a<b?a:b;
+}
+
+/*----------------------------------------------------------------------------------------*/
+
+//函数调用的四种方法
+/*作为函数直接调用，（非严格模式下this表示window，严格模式下是undefined）*/
+function foo(){
+    console,log("log");
+}
+foo();//定义到window下，作为window的方法
+/*作为对象的方法调用（this表示调用此方法的对象）*/
+var x = 45;
+var test = function(){
+    console.log("this.x");
+}
+var obj = {
+    x:23,
+}
+obj.test = test;
+obj.test();//23
+test();//45
+//
+var x = 45;
+var obj = {
+	x:23,
+	test:function(){
+		function foo(){
+			console.log(this.x);
+		}
+		foo();
+	}
+};
+obj.test;//45
+/*通过call()和apply()间接调用（this为函数对象的call//apply方法的首个参数，移花接木）*/
+objA = {name:"AA"};
+objB = {name:"BB"};
+var name = CC;
+objA.foo = function(){
+    console.log(this.name);
+}
+objA.foo.call(objB);//BB;foo后面不能加()，如果加了直接调用objA的方法然后结束
+objA.foo.call(null);//CC;null表示调用全局的name
+
+
+/*----------------------------------------------------------------------------------------*/
+
+//函数参数数量问题
+/*类数组对象*/
